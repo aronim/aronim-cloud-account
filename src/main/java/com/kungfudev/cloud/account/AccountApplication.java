fb6f1bf25ca9c285a4510a1d38bab66e07ab3792
Kevin Sewell <kevin.w.sewell@gmail.com>
2015-05-31 01:41:09 +0200

    Added registry, gateway, authentication

---
+++ src/main/java/com/kungfudev/cloud/account/AccountApplication.java
@@ -0,0 +1,17 @@
+package com.kungfudev.cloud.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * User: Kevin W. Sewell
+ * Date: 2015-05-30
+ * Time: 18h06
+ */
+@SpringBootApplication
+public class AccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class);
+    }
+}
