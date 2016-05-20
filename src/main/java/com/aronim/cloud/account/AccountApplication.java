80af13d380ddd3e55fd3b78505b342f1b60c0b36
Kevin Sewell <kevin.w.sewell@gmail.com>
2016-05-20 16:57:35 +0530

    Migrating kungfudev to aronim

--- src/main/java/com/aronim/cloud/account/AccountApplication.java
+++ src/main/java/com/aronim/cloud/account/AccountApplication.java
@@ -1,4 +1,4 @@
-package com.kungfudev.cloud.account;
+package com.aronim.cloud.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
